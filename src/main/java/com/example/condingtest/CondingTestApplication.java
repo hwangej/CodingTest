458641e8c52c25c1c47a1b95d0af01df15e4c833
hwangej <hij3360@naver.com>
2022-01-17 23:19:40 +0900

    2022-01-17 황은지 git init

---
+++ src/main/java/com/example/condingtest/CondingTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.condingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CondingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/condingtest/CondingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.condingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
