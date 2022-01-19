50d18d4072ca2eb8d6c5a2f84344969486d4d0b3
hwangej <hij3360@naver.com>
2022-01-19 23:51:21 +0900

    2022-01-19 hwangej Baekjoon #1008 ADivisionB

---
+++ src/main/java/com/example/condingtest/B_1008_ADivisionB.java
@@ -0,0 +1,23 @@
+package com.example.condingtest;
+
+import java.util.Scanner;
+
+public class B_1008_ADivisionB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double a,b;
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        System.out.println(a / b);
+    }
+}
+
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+ */
