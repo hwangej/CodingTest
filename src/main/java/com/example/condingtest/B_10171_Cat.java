e45c31413dd62248dcac6b89db7f13c89d7b8024
hwangej <hij3360@naver.com>
2022-01-19 22:53:48 +0900

    2022-01-19 hwangej Baekjoon #10171 Cat

---
+++ src/main/java/com/example/condingtest/B_10171_Cat.java
@@ -0,0 +1,20 @@
+package com.example.condingtest;
+
+public class B_10171_Cat {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
+
+
+    /*
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+ */
