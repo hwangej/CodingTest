3c1d76cfb560aef72e948f2a7e60c9ad6533a09e
hwangej <hij3360@naver.com>
2022-01-19 23:23:17 +0900

    2022-01-19 hwangej Baekjoon #1000 APlusB

---
+++ src/main/java/com/example/condingtest/B_1000_APlusB.java
@@ -0,0 +1,23 @@
+package com.example.condingtest;
+
+import java.util.Scanner;
+
+public class B_1000_APlusB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
+
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+ */
