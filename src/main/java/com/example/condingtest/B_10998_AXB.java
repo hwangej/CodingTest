66429b2d074fe555b13a4045f3595fdd1579eca2
hwangej <hij3360@naver.com>
2022-01-19 23:47:23 +0900

    2022-01-19 hwangej Baekjoon #10998 AXB

---
+++ src/main/java/com/example/condingtest/B_10998_AXB.java
@@ -0,0 +1,27 @@
+package com.example.condingtest;
+
+import java.util.Scanner;
+
+public class B_10998_AXB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a * b);
+    }
+}
+
+
+
+
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+ */
+
