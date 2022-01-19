b0c826d5dfeadc92a4c9210567d68e69d35be6f8
hwangej <hij3360@naver.com>
2022-01-19 22:02:13 +0900

    2022-01-19 황은지 백준 #2557 HelloWorld

---
+++ src/main/java/com/example/condingtest/B_2557_HelloWorld.java
@@ -0,0 +1,19 @@
+package com.example.condingtest;
+
+public class B_2557_HelloWorld {
+    public static void main(String[] args) {
+        // Hello World! 출력
+        System.out.println("Hello World!");
+    }
+}
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+ */
