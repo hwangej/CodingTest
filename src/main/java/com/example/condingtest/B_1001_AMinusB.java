c5f847926e67dd8ff4b630ec8202b14e3c5b27b6
hwangej <hij3360@naver.com>
2022-01-19 23:35:01 +0900

    2022-01-19 hwangej Baekjoon #1001 AMinusB

--- src/main/java/com/example/condingtest/B_1001_AMinusB.java
+++ src/main/java/com/example/condingtest/B_1001_AMinusB.java
@@ -2,14 +2,14 @@ package com.example.condingtest;
 
 import java.util.Scanner;
 
-public class B_1000_APlusB {
+public class B_1001_AMinusB {
     public static void main(String[] args) {
-        //문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+        //문제 : 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
         Scanner sc = new Scanner(System.in);
         int a,b;
         a = sc.nextInt();
         b = sc.nextInt();
-        System.out.println(a + b);
+        System.out.println(a - b);
     }
 }
 
@@ -22,3 +22,4 @@ public class Main {
     }
 }
  */
+
