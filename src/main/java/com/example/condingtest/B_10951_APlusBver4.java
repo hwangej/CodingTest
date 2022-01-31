352dd66d2dbe6ca023ec0275ae42f07506a8c873
hwangej <hij3360@naver.com>
2022-01-31 23:57:52 +0900

    2022-01-31 hwangej BaekJoon 10951_APlusBver4

---
+++ src/main/java/com/example/condingtest/B_10951_APlusBver4.java
@@ -0,0 +1,21 @@
+package com.example.condingtest;
+
+import java.util.Scanner;
+
+public class B_10951_APlusBver4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
+/*
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+ */
