0b2af143b4f25dcf76cfce2026362727002d8995
hwangej <hij3360@naver.com>
2022-02-06 05:56:03 +0900

    2022-02-06 hwangej BaekJoon 10872_Recursion_Factorial_v.0.2

--- src/main/java/com/example/condingtest/B_10872_Recursion_Factorial.java
+++ src/main/java/com/example/condingtest/B_10872_Recursion_Factorial.java
@@ -6,12 +6,18 @@ import java.io.InputStreamReader;
 
 public class B_10872_Recursion_Factorial {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int a = Integer.parseInt(br.readLine());
-        int result = 1;
-        for (int i = 1; i <= a; i++){
-            result *= i;
+
+        int sum = factorial(a);
+        System.out.println(sum);
         }
-        System.out.println(result);
+
+        public static int factorial(int a){
+            if (a <= 1) return 1;
+            return a * factorial(a-1);
+
     }
 }
