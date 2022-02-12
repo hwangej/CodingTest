7f7b68b92355fa385b131383da477d592681ff29
hwangej <hij3360@naver.com>
2022-02-12 20:22:51 +0900

    2022-02-12 hwangej BaeJoon 2908_String_FILIP

---
+++ src/main/java/com/example/condingtest/B_2908_String_FILIP.java
@@ -0,0 +1,26 @@
+package com.example.condingtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_2908_String_FILIP {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        //String[] str = br.readLine().split(" "); // 상수 입력
+
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        if (a > b){
+            System.out.println(a);
+        }else {
+            System.out.println(b);
+        }
+
+
+    }
+}
