71272a392dc202d1a273eb7725680bf9388fec56
hwangej <hij3360@naver.com>
2022-02-19 20:32:06 +0900

    2022-02-19 hwangej BaekJoon 11720_SumOfNum

---
+++ src/main/java/com/example/condingtest/B_11720_SumOfNum.java
@@ -0,0 +1,22 @@
+package com.example.condingtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_11720_SumOfNum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+        String b = br.readLine();
+        int sum = 0;
+
+        for (int i = 0; i < count; i++){
+
+            sum += b.charAt(i)-48; //문자 아스키값에서 숫자 0의 아스키 값 48 빼주기
+        }
+        System.out.println(sum);
+    }
+
+}
